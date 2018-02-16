f29568cf79527e34639829211843b41d2a49937c
wlegolas <wesshead@gmail.com>
2018-02-16 17:39:44 -0200

    Add style for simulate the available/unvailable cells

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,28 +1,69 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { handleEvent, subscribeSelectCell } from '../../api';
+
+const containerCls = 'qm-event-container';
+const cellSlotCls = 'qm-event-slot';
 
 class EventList extends React.Component {
+  constructor({props, state}) {
+    super(props);
+
+    subscribeSelectCell( this.onSubscribeSelectCell );
+  };
+
+  componentDidMount() {
+    const container = document.getElementsByClassName(containerCls)[0];
+
+    container.addEventListener('click', this.handleClick, false);
+  };
+
+  handleClick = (event) => {
+    if ( event.target.classList.contains( cellSlotCls ) ) {
+        this.onSelect(event.target);
+    }
+
+    event.stopPropagation();
+  };
+
+  onSubscribeSelectCell = (cellId) => {
+    this.lockCell(cellId);
+  };
+
+  onSelect = (cell) => {
+    handleEvent('select-cell', cell.id);
+  };
+
+  lockCell = (cellId) => {
+    const cellEl = document.getElementById(cellId);
+
+    if (cellEl) {
+      console.log(`Received the cell id ${ cellId }`);
+      cellEl.classList.add('qm-cell-unvaliable');
+    }
+  };
+
   render() {
     return (
-      <Container className="qm-event-container">
+      <Container className={ containerCls }>
         <Row>
           <Col className="qm-event-title">Fri, Feb 16</Col>
         </Row>
         <Row>
           <Col>9:30</Col>
-          <Col>user_1</Col>
+          <Col className={ cellSlotCls } id="cell_1">user_1</Col>
         </Row>
         <Row>
           <Col>9:45</Col>
-          <Col>user_2</Col>
+          <Col className={ cellSlotCls } id="cell_2">user_2</Col>
         </Row>
         <Row>
           <Col>10:00</Col>
-          <Col>user_3</Col>
+          <Col className={ cellSlotCls } id="cell_3">user_3</Col>
         </Row>
         <Row>
           <Col>10:15</Col>
-          <Col className="disabled"></Col>
+          <Col className="qm-cell-disabled"></Col>
         </Row>
       </Container>
     )
